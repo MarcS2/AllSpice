4303f798393893b0c9fd0ee3117eac0678f79d9b
MarcS2 <marcellosharrai@hotmail.com>
2023-11-30 10:23:52 -0700

    AllSpice

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,19 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS recipes(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title CHAR(255) NOT NULL,
+        instructions CHAR(255) NOT NULL,
+        img VARCHAR(1000) NOT NULL,
+        category CHAR(255) NOT NULL,
+        creatorId VARCHAR(255) NOT NULL
+    ) default charset utf8 COMMENT '';
